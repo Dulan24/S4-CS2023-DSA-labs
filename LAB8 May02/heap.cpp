03dd62acd333cae2fba66104c9f0d5fe8c069ecc
Dulan24 <Dulanlokugeegana@gmail.com>
2023-05-02 20:29:44 +0530

    update lab8

--- LAB8 May02/heap.cpp
+++ LAB8 May02/heap.cpp
@@ -1,58 +1,6 @@
 #include <iostream>
 using namespace std;
   
-// function to heapify the tree
-void heapify(int arr[], int n, int root)
-{
-   // build heapify
-   if(root==0){
-      return;
-   }
-   if(root%2==1){ // left root
-      if(arr[root]>arr[root/2]){
-         int temp = arr[root/2];
-         arr[root/2] = arr[root];
-         arr[root] = temp;
-         heapify(arr, n, root/2);
-      }
-   
-   }
-   else{ // right root
-      int max = arr[root-1];
-      int maxindex = root-1;
-      if (arr[root]>arr[root-1]){
-         max = arr[root];
-         maxindex = root;
-      }
-      if (arr[maxindex]>arr[(maxindex-1)/2]){  
-         int temp = arr[(maxindex-1)/2];
-         arr[(maxindex-1)/2] = arr[maxindex];
-         arr[maxindex] = arr[(maxindex-1)/2];
-         heapify(arr, n, (maxindex-1)/2);
-      }
-      
-   }
-   
-   
-}
-  
-// implementing heap sort
-void heapSort(int arr[], int n)
-{
-   // build heap
-   for(int i=1; i<n; i++){
-      heapify(arr, n, i);
-   }
-  
-   // extracting elements from heap one by one
-   for (int i=0; i<n; i++){
-      int temp = arr[n-1-i];
-      arr[n-1-i] = arr[0];
-      arr[0] = temp;
-      heapify(arr, i, 0);
-   }
-}
-  
 /* print contents of array */
 void displayArray(int arr[], int n)
 {
@@ -60,11 +8,78 @@ void displayArray(int arr[], int n)
    cout << arr[i] << " ";
    cout << "\n";
 }
+
+// function to heapify the tree
+void heapify(int arr[], int n, int root){
+   // build heapify
+   if(2*root+1>=n && 2*root+2>=n){
+      return;
+   }
+   if(2*root+1>=n){
+      if(arr[root]<arr[2*root+1]){
+         int temp = arr[2*root+1];
+         arr[2*root+1] = arr[root];
+         arr[root] = temp;
+         return heapify(arr, n, 2*root+1);
+      }
+   }
+   else{ 
+      if(arr[2*root+1]>arr[2*root+2]){
+         if(arr[root]<arr[2*root+1]){
+            int temp = arr[2*root+1];
+            arr[2*root+1] = arr[root];
+            arr[root] = temp;
+            return heapify(arr, n, arr[2*root+1]);
+         }
+      }
+      else{
+         if(arr[root]<arr[2*root+2]){
+            int temp = arr[2*root+2];
+            arr[2*root+2] = arr[root];
+            arr[root] = temp;
+            return heapify(arr, n, arr[2*root+2]);
+         }
+      }
+   }
+}
+  
+// implementing heap sort
+void heapSort(int arr[], int n)
+{
+   // build heap
+   for(int i=1; i<n; i++){
+      int j = i;
+      while ((j-1)/2>=0){
+         if(arr[j]>arr[(j-1)/2]){
+            int temp = arr[(j-1)/2];
+            arr[(j-1)/2] = arr[j];
+            arr[j] = temp;
+         }
+         j = (j-1)/2;
+         if (j==0){
+            break;
+         }
+      }
+   }
+   //displayArray(arr, n);
+   // extracting elements from heap one by one
+   for (int i=0; i<n; i++){
+      int temp = arr[n-1-i];
+      arr[n-1-i] = arr[0];
+      arr[0] = temp;
+      heapify(arr, n-1-i, 0);
+      //displayArray(arr,n);
+   }
+   
+}
+  
+
+
   
 // main program
 int main()
 {
-   int heap_arr[] = {4,17,3,12,9,6};
+   int heap_arr[] = {4,17,3,12,9,6,56,4};
    int n = sizeof(heap_arr)/sizeof(heap_arr[0]);
    cout<<"Input array"<<endl;
    displayArray(heap_arr,n);
